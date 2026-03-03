63503c0701d4ae1ca3af67b3af2c361f40fa96cf
Aluno Engenharia Software <alunoes@aeelabs.edu.br>
2026-03-03 19:56:52 -0300

    fix the page with not content

---
+++ app/base-de-conhecimento/page.tsx
@@ -0,0 +1,7 @@
+export default function BaseDeConhecimento(){
+    return(
+        <>
+        BASE DE CONHECIMENTO (WORKING PAE)
+        </>
+    )
+}
---
+++ app/nao-respondidas/page.tsx
@@ -0,0 +1,7 @@
+export default function NaoRespondidas(){
+    return(
+        <>
+        NaoRespondidas (WORKING PAE)
+        </>
+    )
+}
